c9da92e06c1c6038cb5fd576296803fc1643176d
Jeremy Lynn <113862439+JLynnCode@users.noreply.github.com>
2022-12-14 11:05:31 -0600

    Added additional projects to work card data file

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -8,20 +8,74 @@ const projectCardData = [
         imgsrc: twitterimg,
         title: "Twitter Clone",
         text: "Collaborative work using the Spring Java framework to create a RESTful Web Service which replicated basic twitter functionality via CRUD operations on a PostgreSQL database.",
-        view: "https://github.com/fasttrackd-student-work/spring-assessment-social-media-sprint-10-team-3"
+        view: "https://github.com/JLynnCode/Spring-Twitter-Clone"
     },
     {
         imgsrc: quizimg,
         title: "Quiz API",
         text: "Created an API for a provided Quiz skeleton, adding backend functionality for performing CRUD operations that allow users to create, modify, and retrieve quizzes.",
-        view: "https://github.com/fasttrackd-student-work/spring-assignment-quiz-api-JLynnCode"
+        view: "https://github.com/JLynnCode/Spring-Quiz-API"
     },
     {
         imgsrc: orgimg,
         title: "Employee Org Chart",
         text: "Java application that has multiple methods of sorting/retrieving a list of Employees and Managers as a demonstrative understanding of abstraction & inheritance.",
-        view: "https://github.com/fasttrackd-student-work/java-assignment-collections-JLynnCode"
-    }
+        view: "https://github.com/JLynnCode/JavaCollections-OrgChart"
+    },
+    {
+        imgsrc: orgimg,
+        title: "Company Portal",
+        text: "",
+        view: "https://github.com/JLynnCode/Company-Portal"
+    },
+    {
+        imgsrc: orgimg,
+        title: "Spotify Guessing Game",
+        text: "",
+        view: "https://github.com/JLynnCode/Spotify-Guessing-Game"
+    },
+    {
+        imgsrc: orgimg,
+        title: "Lemonade Stand",
+        text: "",
+        view: "https://github.com/JLynnCode/Spring-Lemonade-Stand"
+    },
+    {
+        imgsrc: orgimg,
+        title: "Rational Numbers",
+        text: "",
+        view: "https://github.com/JLynnCode/Rationals"
+    },
+        {
+        imgsrc: orgimg,
+        title: "FizzBuzz & Fibonacci",
+        text: "",
+        view: "https://github.com/JLynnCode/Fibonacci-FizzBuzz"
+    },
+        {
+        imgsrc: orgimg,
+        title: "SQL Query Exercise",
+        text: "",
+        view: "https://github.com/JLynnCode/SQL-Query-Exercise"
+    },
+    {
+        imgsrc: orgimg,
+        title: "Declarative Exercise",
+        text: "",
+        view: "https://github.com/JLynnCode/Declarative-Style-Demo"
+    },
+    {
+        imgsrc: orgimg,
+        title: "Functional Exercise",
+        text: "",
+        view: "https://github.com/JLynnCode/Declarative-Style-Demo"
+    },
+    {
+        imgsrc: orgimg,
+        title: "Quizler",
+        text: "",
+        view: "https://github.com/JLynnCode/Quizler"
+    },
 ]
 
 export default projectCardData;
