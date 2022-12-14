55d4758744a570af8b2b5d848b830364efbdb06f
Jeremy Lynn <113862439+JLynnCode@users.noreply.github.com>
2022-12-14 11:23:58 -0600

    Added descriptions to newly populated Work Cards

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -7,7 +7,7 @@ const projectCardData = [
     {
         imgsrc: twitterimg,
         title: "Twitter Clone",
-        text: "Collaborative work using the Spring Java framework to create a RESTful Web Service which replicated basic twitter functionality via CRUD operations on a PostgreSQL database.",
+        text: "Collaborative work to create a RESTful Web Service which replicated basic twitter functionality via CRUD operations on a PostgreSQL database.",
         view: "https://github.com/JLynnCode/Spring-Twitter-Clone"
     },
     {
@@ -25,57 +25,57 @@ const projectCardData = [
     {
         imgsrc: orgimg,
         title: "Company Portal",
-        text: "",
+        text: "Full Stack application created between two teams to emulate a prototype employee portal.",
         view: "https://github.com/JLynnCode/Company-Portal"
     },
     {
         imgsrc: orgimg,
         title: "Spotify Guessing Game",
-        text: "",
+        text: "Utilizes artist & track data fetched from the Spotify API to create a musical guessing game.",
         view: "https://github.com/JLynnCode/Spotify-Guessing-Game"
     },
     {
         imgsrc: orgimg,
         title: "Lemonade Stand",
-        text: "",
+        text: "A basic Lemonade Stand application with various functionality to create, track, and revise orders.",
         view: "https://github.com/JLynnCode/Spring-Lemonade-Stand"
     },
     {
         imgsrc: orgimg,
         title: "Rational Numbers",
-        text: "",
+        text: "Performs various mathematical operations on rational numbers, including simplification.",
         view: "https://github.com/JLynnCode/Rationals"
     },
         {
         imgsrc: orgimg,
         title: "FizzBuzz & Fibonacci",
-        text: "",
+        text: "The traditional 'FizzBuzz' exercise, and another for performings operations on a Fibonacci sequence.",
         view: "https://github.com/JLynnCode/Fibonacci-FizzBuzz"
     },
         {
         imgsrc: orgimg,
         title: "SQL Query Exercise",
-        text: "",
+        text: "Simple exercise working with complex SQL queries in preparation for back-end DB management.",
         view: "https://github.com/JLynnCode/SQL-Query-Exercise"
     },
     {
         imgsrc: orgimg,
         title: "Declarative Exercise",
-        text: "",
+        text: "Exercise to demonstrate a 'Declarative Programming' design pattern.",
         view: "https://github.com/JLynnCode/Declarative-Style-Demo"
     },
     {
         imgsrc: orgimg,
         title: "Functional Exercise",
-        text: "",
+        text: "Exercise to demonstrate a 'Functional Programming' design pattern.",
         view: "https://github.com/JLynnCode/Declarative-Style-Demo"
     },
     {
         imgsrc: orgimg,
         title: "Quizler",
-        text: "",
+        text: "Created a custom library for a provided CLI, allowing user to create & take quizzes.",
         view: "https://github.com/JLynnCode/Quizler"
-    },
+    }
 ]
 
 export default projectCardData;
